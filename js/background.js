091d458e5283117e2e786ad85d865085bf4debe6
James Wright <james@jamesswright.co.uk>
2015-08-31 19:06:22 +0100

    WIP rewinding of background

--- js/background.js
+++ js/background.js
@@ -2,6 +2,19 @@
 	'use strict';
 	var ctx = document.querySelector('#background').getContext('2d');
 	var background = generateBackground();
+	var decelerationRate = 0;
+	var isRewinding = false;
+
+	jw.events.register('bg', background);
+
+	function onRewindStart() {
+		isRewinding = true;
+	}
+
+	function onPlay() {
+		isRewinding = false;
+		decelerationRate = 0;
+	}
 
 	function nextFrame(timestamp) {
 		animateBackground();
@@ -25,19 +38,28 @@
 
 		return {
 			starFill: 'white',
-			stars: stars
+			stars: stars,
+			onRewindStart: onRewindStart,
+			onPlay: onPlay
 		};
 	}
 
 	function animateBackground() {
+		if (isRewinding && decelerationRate < 8)
+			decelerationRate += 0.5;
+
 		ctx.fillStyle = background.starFill;
 
 		background.stars.forEach(function (star) {
 			ctx.clearRect(star.x, star.y, star.size, star.size);
 			
-			star.x -= star.speed;
+			star.x -= star.speed - decelerationRate;
+
 			if (star.x + star.size < 0)
 				star.x = jw.gameWidth + star.size;
+
+			if (star.x > jw.gameWidth + star.size)
+				star.x = 0 - star.size;
 			
 			ctx.fillRect(star.x, star.y, star.size, star.size);
 		});
