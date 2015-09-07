80b305688ad605d48cbb641a7c31d65351ad9849
James Wright <james@jamesswright.co.uk>
2015-09-07 23:12:03 +0100

    WIP title screen

---
+++ js/title.js
@@ -0,0 +1,22 @@
+(function () {
+	'use strict';
+
+	var canvas = document.querySelector('#title');
+	var ctx = canvas.getContext('2d');
+	var primaryFontSize = 48;
+	var subtitleFontSize = 32;
+	var fontFamily = 'Courier New';
+
+	window.addEventListener('load', function () {
+		requestAnimationFrame(function () {
+			ctx.font = primaryFontSize + 'px ' + fontFamily;
+			ctx.fillStyle = 'white';
+			ctx.fillText('Time Worm!', jw.gameWidth / 3, (jw.gameHeight / 2) - primaryFontSize);
+		
+			ctx.font = subtitleFontSize + 'px ' + fontFamily;
+			ctx.fillText('(c) James Wright 2015', (jw.gameWidth / 3) -70, (jw.gameHeight / 2) - subtitleFontSize + primaryFontSize);
+			ctx.fillText('jamesswright.co.uk', (jw.gameWidth / 3) -35, (jw.gameHeight / 2) + 30 + primaryFontSize);
+			ctx.fillText('Click to play!', (jw.gameWidth / 3) + 5, (jw.gameHeight / 2) + 30 + primaryFontSize + subtitleFontSize * 2);
+		});
+	});
+}());
